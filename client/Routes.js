325f12bf4f196a28479bc6812056f0b1970bf727
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-01 17:52:35 -0400

    added nav and footer route

--- client/Routes.js
+++ client/Routes.js
@@ -3,13 +3,17 @@ import { withRouter, Switch, Route } from "react-router-dom";
 
 //ROUTES
 import Home from "./components/Home";
+import Games from "./components/Games";
+import Websites from "./components/Websites";
 
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route path="/games" component={Games} />
+          <Route path="/websites" component={Websites} />
         </Switch>
       </div>
     );
