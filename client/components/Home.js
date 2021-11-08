cca39de6727c1e8ab7eacb2b92f0015a6c584deb
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-08 12:25:05 -0500

    completed home page with two routes to projects

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,4 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+} from "@mui/material";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import WebOutlinedIcon from "@mui/icons-material/WebOutlined";
 
 class Home extends Component {
   constructor() {
@@ -7,9 +19,114 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          alignItems: "stretch",
+          textAlign: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "100%", md: "calc(50% - 5vh)" },
+            boxShadow: 15,
+          }}
+        >
+          <Card
+            sx={{
+              height: "100%",
+              backgroundColor: "#30302f",
+              boxShadow: 10,
+              transition: "transform 0.15s ease-in-out",
+              "&:hover": {
+                transform: "scale3d(1.05, 1.05, 1)",
+              },
+              paddingBottom: "25px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              alt="pacman game view"
+              height="75%"
+              image="https://i.imgur.com/wFlMDyY.png"
+            />
+
+            <CardContent sx={{ marginBottom: "-10px" }}>
+              <Typography variant="h6">GAMES</Typography>
+              <Typography variant="subtitle1">
+                STEP INTO MY PIXEL PERFECT WORLD OF GAMES
+              </Typography>
+            </CardContent>
+
+            <CardActions style={{ justifyContent: "center" }}>
+              <Link to={"/games"} style={{ textDecoration: "none" }}>
+                <Button
+                  startIcon={<SportsEsportsOutlinedIcon />}
+                  variant="outlined"
+                  color="primary"
+                  style={{
+                    fontSize: "large",
+                  }}
+                >
+                  VIEW PROJECTS
+                </Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Box>
+
+        <Box
+          sx={{
+            width: { xs: "100%", md: "calc(50% - 5vh)" },
+            boxShadow: 15,
+          }}
+        >
+          <Card
+            sx={{
+              height: "100%",
+              backgroundColor: "#30302f",
+              boxShadow: 10,
+              transition: "transform 0.15s ease-in-out",
+              "&:hover": {
+                transform: "scale3d(1.05, 1.05, 1)",
+              },
+              paddingBottom: "25px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              alt="pacman game view"
+              height="75%"
+              image="https://i.imgur.com/ItDUWXc.png"
+            />
+
+            <CardContent sx={{ marginBottom: "-10px" }}>
+              <Typography variant="h6">WEBSITES</Typography>
+              <Typography variant="subtitle1">
+                WECOME TO THE LAND OF CREATIVITY
+              </Typography>
+            </CardContent>
+
+            <CardActions style={{ justifyContent: "center" }}>
+              <Link to={"/websites"} style={{ textDecoration: "none" }}>
+                <Button
+                  endIcon={<WebOutlinedIcon />}
+                  variant="outlined"
+                  color="primary"
+                  style={{
+                    // color: "#121111",
+                    fontSize: "large",
+                  }}
+                >
+                  VIEW PROJECTS
+                </Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Box>
+      </Box>
     );
   }
 }
