0497d52ac8ef0d9618d3ae2adf4f79a3a4fc717a
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-08 12:20:28 -0500

    added all routes

--- client/Routes.js
+++ client/Routes.js
@@ -3,8 +3,10 @@ import { withRouter, Switch, Route } from "react-router-dom";
 
 //ROUTES
 import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
 import Games from "./components/Games";
 import Websites from "./components/Websites";
+import NotFound from "./components/NotFound";
 
 class Routes extends Component {
   render() {
@@ -12,8 +14,10 @@ class Routes extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/aboutme" component={AboutMe} />
           <Route path="/games" component={Games} />
           <Route path="/websites" component={Websites} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     );
