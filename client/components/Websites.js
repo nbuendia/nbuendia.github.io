9d535d2be1abf81f02a01e33fd974627540c7412
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-08 12:26:48 -0500

    website page under constuction but currently displaying a comming soon msg

--- client/components/Websites.js
+++ client/components/Websites.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Box, Typography } from "@mui/material";
 
 class Websites extends Component {
   constructor() {
@@ -7,9 +8,24 @@ class Websites extends Component {
 
   render() {
     return (
-      <div>
-        <h1>LOOK AT ALL MY WEBSITES!</h1>
-      </div>
+      <Box sx={{ textAlign: "center" }}>
+        <Typography
+          variant="h1"
+          sx={{
+            color: "black",
+            // fontFamily: "Architects Daughter",
+            textShadow: "1px 1px 10px magenta",
+            opacity: "0.7",
+            fontSize: {
+              xs: "50px",
+              md: "75px",
+              lg: "100px",
+            },
+          }}
+        >
+          MORE PROJECTS COMING SOON!
+        </Typography>
+      </Box>
     );
   }
 }
