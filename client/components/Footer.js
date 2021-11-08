dfbc6a7f248ed09d68fc08c21c0464cd85b04dec
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-08 12:21:58 -0500

    completed footer, with social media links and fixed position to bottom

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Tooltip,
+  Fade,
+  Button,
+  Link,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 class Footer extends Component {
   constructor() {
@@ -7,9 +18,45 @@ class Footer extends Component {
 
   render() {
     return (
-      <div>
-        <h1>I AM A FOOTER!</h1>
-      </div>
+      <Box>
+        <AppBar>
+          <Toolbar
+            style={{
+              display: "flex",
+              backgroundColor: "#30302f",
+              justifyContent: "space-evenly",
+              position: "fixed",
+              bottom: 0,
+              width: "100%",
+              height: "100px",
+            }}
+          >
+            <Tooltip title="LINKEDIN PROFILE" TransitionComponent={Fade}>
+              <Link
+                href="https://www.linkedin.com/in/nicole-buendia/"
+                target="_blank"
+                rel="noopener"
+              >
+                <Button color="info">
+                  <LinkedInIcon style={{ fontSize: 35 }} />
+                </Button>
+              </Link>
+            </Tooltip>
+
+            <Tooltip title="GITHUB PROFILE" TransitionComponent={Fade}>
+              <Link
+                href="https://github.com/nbuendia"
+                target="_blank"
+                rel="noopener"
+              >
+                <Button color="info">
+                  <GitHubIcon style={{ fontSize: 35 }} />
+                </Button>
+              </Link>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+      </Box>
     );
   }
 }
