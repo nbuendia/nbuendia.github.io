e448e2bcd57cb632311da545d39937e8b9620f7c
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-08 12:18:38 -0500

    added mui theme and changed app format

--- client/App.js
+++ client/App.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { Container } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 //PAGE CONTENTS
 import Routes from "./Routes";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00FFFF", //CYAN
+    },
+    secondary: {
+      main: "#FF00FF", //MAGENTA
+    },
+    info: {
+      main: "#121111", //BLACKish
+    },
+    text: {
+      primary: "#dbdbdb", //OFFWHITE
+    },
+  },
+  typography: {
+    fontFamily: "Architects Daughter",
+    fontWeightRegular: "400",
+  },
+});
+
 function App() {
   return (
-    <div>
-      <Nav />
-      <Routes />
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container sx={{ marginTop: "125px", marginBottom: "125px" }}>
+        <Nav />
+        <Routes />
+        <Footer />
+      </Container>
+    </ThemeProvider>
   );
 }
 
